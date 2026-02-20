14a3441a7b26f3fc412c794eb35281474edf46eb
Eliz30ttt <elizwilson321@gmail.com>
2026-02-20 10:07:48 -0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -10,6 +10,5 @@ const firebaseConfig = {
   appId: "1:419423769358:web:67c2f7ab8f5f60222dbd5f"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
